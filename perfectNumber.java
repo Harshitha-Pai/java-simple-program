634193b3d2eb019311b28dacfd0724d19511adc7
Harshitha A Pai <137300653+Harshitha-Pai@users.noreply.github.com>
2025-08-01 15:29:12 +0530

    Create perfectNumber.java

---
+++ perfectNumber.java
@@ -0,0 +1,25 @@
+package training;
+import java.util.Scanner;
+
+public class Training {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+	    int n= sc.nextInt();
+	    int sum=0;
+	    int i;
+	    
+	    for(i=1;i<n;i++) {
+	    	if(n%i==0) {
+	    		sum=sum+i;
+	    	}
+	    }
+	    
+	    if(sum==n) {
+			System.out.println("The given number is perfect");
+	    }else {
+			System.out.println("The given number is not perfect");
+
+	    }
+	}
+}
