9719319f12bd5cbb54ba1a42284da5772a98ff71
Harshitha A Pai <137300653+Harshitha-Pai@users.noreply.github.com>
2025-08-02 11:24:57 +0530

    Create HappyNumber.java

---
+++ HappyNumber.java
@@ -0,0 +1,30 @@
+package training;
+import java.util.Scanner;
+//sum of n natural number
+public class Training {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number n");
+	    int n= sc.nextInt();
+	    int sum=0;
+	    
+	    while(n!=0) {
+	    	int rem=n%10;
+	    	sum=sum+rem*rem;
+	    	n=n/10;
+	    	if(n==0) {
+	    		if(sum>9) {
+	    			n=sum;
+	    			sum=0;
+	    		}
+	    	}
+	    }
+	    
+	    if(sum==1) {
+	    	System.out.println("is a happy number");
+	    }else {
+	    	System.out.println("Not a happpy number");
+	    }
+	}   
+}
+
